ba3dd31dea9a1f06a12c82f7f21014d5b2114fab
Daniel <rotwells16@gmail.com>
2023-11-03 21:54:52 -0500

    ent3-31gen

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import InfoLocation from "./components/InfoLocation";
+import CardResident from "./components/CardResident";
+
+function App() {
+  const [locationId, setLocationId] = useState(
+    Math.floor(Math.random() * 126) + 1
+  );
+
+  const url = `https://rickandmortyapi.com/api/location/${locationId}`;
+  const [location, getLocation, isLoading, hasError] = useFetch(url);
+
+  useEffect(() => {
+    getLocation();
+  }, [locationId]);
+
+  const inputLocation = useRef();
+
+  const handleLocation = (e) => {
+    e.preventDefault();
+    setLocationId(inputLocation.current.value.trim());
+  };
+  return (
+    <div className="app">
+      <h1 className="app_title">Rick and Morty</h1>
+      <form className="app_form" onSubmit={handleLocation}>
+        <input className="app_input" ref={inputLocation} type="text" />
+        <button className="app_btn">Search</button>
+      </form>
+      {isLoading ? (
+        <h2>Loading ...</h2>
+      ) : hasError || locationId === "0" ? (
+        <h2> ❌ Hey! you must provide an id from 1 to 126</h2>
+      ) : (
+        <>
+          <InfoLocation location={location} />
+          <div className="app__card-container">
+            {location?.residents.map((url) => (
+              <CardResident key={url} url={url} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
